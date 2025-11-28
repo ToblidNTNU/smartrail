d92428ac0464e6238f7e6d5a717c3a3baac009f5
larswdagestad <larswdagestad@gmail.com>
2025-11-28 10:20:16 +0100

    med ping

--- smart_train/src/Recive.cpp
+++ smart_train/src/Recive.cpp
@@ -1,32 +1,59 @@
 #include <esp_now.h>
 #include <WiFi.h>
+uint8_t broadcastAddress[] = {0x38, 0x18, 0x2b, 0x2f, 0x56, 0x50};
+
+esp_now_peer_info_t peerInfo;
 
 const int RedLED = 2;
 unsigned long Time;
 unsigned long OldTime1;
 unsigned long OldTime2;
+unsigned long OldTime3;
 bool Trainstate;
+int i = 0;
+int PingTime; 
 
 // Structure example to receive data
 // Must match the sender structure
 typedef struct struct_message {
     int a;
-    bool b;
+    float x;
+    float y;
+    float z;
+    int knapp;
     
 } struct_message;
 
 // Create a struct_message called myData
 struct_message myData;
+struct_message urData;
+
 
 // callback function that will be executed when data is received
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   memcpy(&myData, incomingData, sizeof(myData));
-  Serial.print("Bytes received: ");
+  //Serial.print("  Bytes received: ");
   Serial.println(myData.a);
-  Serial.print("Int: ");
+  Serial.print("x-akse: ");
+  Serial.print(myData.x);
+  Serial.print("  y-akse: ");
+  Serial.print(myData.y);
+  Serial.print("  z-akse: ");
+  Serial.print(myData.z);
+  Serial.print("  knapp status: ");
+  Serial.println(myData.knapp);
+  PingTime = Time - myData.a;
+  Serial.print("Reply from ESP1, Time=");
+  Serial.print(PingTime);
+  Serial.print(".00ms");
   OldTime1 = Time;
 
 }
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+ 
  
 void setup() {
   // Initialize Serial Monitor
@@ -44,8 +71,22 @@ void setup() {
   // Once ESPNow is successfully Init, we will register for recv CB to
   // get recv packer info
   esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
+  esp_now_register_send_cb(esp_now_send_cb_t(OnDataSent));
+
+ // Register peer
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false; 
+
+    // Add peer 
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+
   pinMode(RedLED, OUTPUT);
-  Trainstate = true;
+
+  Trainstate = false;
 }
  
 void loop() {
@@ -53,6 +94,23 @@ void loop() {
   if(myData.a == 1){
     digitalWrite(RedLED ,HIGH);
     }
+  
+  
+   
+  
+  // Send message via ESP-NOW
+  if(Time - OldTime1 >= 3000){
+      urData.a = Time;
+      esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &urData, sizeof(urData));
+      OldTime1 = Time;      
+  }
+    /*
+    if(Trainstate == false && Time <= 10000 && Time - OldTime3 >= 1000){
+      Serial.println("connecting" + dots*i);
+       i = i + 1;
+       if(i >= 5) i = 1;
+        OldTime3 = Time;
+       }*/
 
   if(myData.a == 0) {
     digitalWrite(RedLED, LOW);
@@ -63,12 +121,13 @@ void loop() {
   if(Time - OldTime1 <= 10000){
     Trainstate = true;
   }
+ 
 
-  if(Trainstate == false && Time - OldTime2 >= 1000){
+  if(Trainstate == false && Time - OldTime2 >= 5000){
     Serial.println("No Connection with train");
      OldTime2 = Time;
   }
-  if(Trainstate == true && Time - OldTime2 >= 1000){
+  if(Trainstate == true && Time - OldTime2 >= 5000){
     Serial.println("Connection with train OK");
      OldTime2 = Time;
   }
